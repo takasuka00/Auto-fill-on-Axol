4f2bb1c2b39d6ba9e1923c5eabb5ab8c3220b597
takasuka00 <takasuka00@gmail.com>
2024-07-05 17:41:05 +0900

    clean コメント付けとリファクタリングの実施

--- scripts/content.ts
+++ scripts/content.ts
@@ -1,25 +1,30 @@
-let IDKey:string
-let PassKey:string
-let idInput : HTMLInputElement
-let passInput : HTMLInputElement
+let IDKey: string
+let PassKey: string
+let idInput: HTMLInputElement
+let passInput: HTMLInputElement
 
-window.onload = function() {
+//アクセス時実行
+window.onload = function () {
+    //企業名の取得
     const companyName = window.location.href.split("/")[5]
     IDKey = companyName + "_ID"
     PassKey = companyName + "_Pass"
     const idDiv = document.querySelector<HTMLDivElement>('.origin__id');
     const passDiv = document.querySelector<HTMLDivElement>('.origin__pass');
     if (idDiv == null) return
-    if(passDiv == null) return
+    if (passDiv == null) return
     idInput = idDiv.firstElementChild as HTMLInputElement
     passInput = passDiv.firstElementChild as HTMLInputElement
-    chrome.storage.sync.get([IDKey,PassKey],function (key) {
-        if (key[IDKey] ==undefined) return
+    //保存してあるIDとパスワードを取得し貼り付け
+    chrome.storage.sync.get([IDKey, PassKey], function (key) {
+        if (key[IDKey] == undefined) return
         idInput.value = key[IDKey]
         passInput.value = key[PassKey]
     })
 }
+//ページを離れる際に実行
 window.onbeforeunload = function () {
-    chrome.storage.sync.set({[IDKey]: idInput.value,[PassKey]: passInput.value}, function () {
+    //IDとパスワードを保存
+    chrome.storage.sync.set({[IDKey]: idInput.value, [PassKey]: passInput.value}, function () {
     })
 }
